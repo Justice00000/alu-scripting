f275226a8337cf975095c6dd0603365f20a6de6d
Justice Chukwuonye <j.chukwuony@alustudent.com>
2023-09-07 17:28:36 -0700

    Loading

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/School/).join
+## Check if the provided argument contains the word "School"
+
+# Ensure that one argument is provided
+if ARGV.length != 1
+  puts "Usage: ruby regex_match.rb <input_string>"
+  exit(1)
+end
+
+# Get the input string from the command-line argument
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /School/
+
+# Use the match method to check for a match
+if input_string.match?(pattern)
+  puts "The provided argument contains the word 'School.'"
+else
+  puts "The provided argument does not contain the word 'School.'"
+end
