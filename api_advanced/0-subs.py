39015c30e7c0d7ee6a45ddf9497997bfe6903ff6
CeeJay <j.chukwuony@alustudent.com>
2023-11-11 20:49:07 +0300

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""""Doc"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """"Doc"""
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """
+    Query the Reddit API and return the number of subscribers of a given subreddit.
 
-    my_headers = {
-        "User-Agent": "LetsGo/0.1 by Justice00101"
-        }
+    Args:
+        subreddit (str): The name of the subreddit.
 
-    raw_response = requests.get(URL, headers=my_headers, allow_redirects=False)
+    Returns:
+        int: The number of subscribers, or 0 if an error occurs.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "4UqdQ42h3yqavQSMPnkdV8OtuI9khA/1.0 by Justice00101"}
 
-    if raw_response.status_code == 200:
-        json_response = raw_response.json()
-        sub_count = json_response['data']['subscribers']
-        return sub_count
-
-    else:
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        return response.json().get("data").get("subscribers")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
         return 0
